769f6969410462669efc97175fc6519fd8a21eec
aryaraju28 <arya28199@gmail.com>
2021-05-03 16:02:53 +0530

    Add project files.

---
+++ EmployeManagementMvc/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EmployeManagementMvc.Models;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace EmployeManagementMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        String connectionString = "Server=RAED_COMPUTER\\SQLEXPRESS;Database=EmployeeManagementSystem;Trusted_Connection=True;";
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(Employee employee)
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+            SqlCommand command = new SqlCommand("EmployeeSave", connection);
+            command.CommandType = System.Data.CommandType.StoredProcedure;
+            command.Parameters.AddWithValue("Name", employee.Name);
+            command.Parameters.AddWithValue("Age", employee.Age);
+            command.Parameters.AddWithValue("Email", employee.Email);
+            command.ExecuteNonQuery();
+            connection.Close();
+            ViewData["Message"] = "Employee Records" + employee.Name + "Is Saved Successfully";
+
+            return View(employee);
+        }
+    }
+}
---
+++ EmployeManagementMvc/Models/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace EmployeManagementMvc.Models
+{
+    public class Employee
+    {
+       [Required(ErrorMessage ="Enter Name")]
+       [Display(Name="Enter Name")]
+       [StringLength(maximumLength:10,MinimumLength =3,ErrorMessage ="Name Length Must Be Maximun 10 & Minimum 3")]
+
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Please Enter Age")]
+        [Range(18,50)]
+        public int Age { get; set; }
+        [Required(ErrorMessage ="Please Enter Email Address")]
+        [Display(Name ="Email Id:")]
+        public string Email { get; set; }
+    }
+}
